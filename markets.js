125fbee1e3cb7ab8325492a612c410b7e7e0f09d
Nitinram Velraj <nitinram1996@gmail.com>
2021-01-13 23:40:24 -0500

    adding the markets page which reads from dummy api

--- markets.js
+++ markets.js
@@ -1,5 +1,5 @@
 async function getQuestions() {
-    let url = 'https://5ff305db28c3980017b18e2a.mockapi.io/dummy/questions';
+    let url = 'https://5fff1998a4a0dd001701b79f.mockapi.io/questions';
     try {
         let res = await fetch(url);
         return await res.json();
@@ -8,24 +8,32 @@ async function getQuestions() {
     }
 }
 
+async function dateToDay(date) {
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let dateSlice = new Date(date.slice(0,10));  
+    let day = days[dateSlice.getDay()];
+    return day
+}
+
 async function renderQuestions() {
     let questions = await getQuestions();
     let html = '';
     questions.forEach(question => {
-        let htmlSegment = `<div class="question">
-        <p id="question-text">${question.question_text}</p>
-        <p id="question-text">${question.explanation_text}</p>
-        <p id="question-text">${question.resolution_rule_text}</p>
-        <p id="question-text">${question.creation_time}</p>
-        <p id="question-text">${question.deadline_for_betting}</p>
-        <p id="question-text">${question.deadline_for_resolving}</p>
+        // console.log(dateToDay(question.creation_time))
+        let htmlSegment = `<div class="well">
+        <h3 id="question">${question.question_text}</h3> 
+        <h6 id="explanation">${question.explanation_text}</h6>
+        <p id="resolution" class="glyphicon glyphicon-time">Question created on:${question.resolution_rule}</p>
+        <p id="time-created">Question created on: ${question.creation_time}</p>
+        <p id="deadline-bet">Deadline for placing bets: ${question.deadline_for_betting}</p>
+        <p id="deadline-resolve">Deadline for resolving question: ${question.deadline_for_resolving}</p>
         </div>`;
 
         html += htmlSegment;
     });
 
-    let container = document.querySelector('marketList');
+    let container = document.getElementById('marketList');
     container.innerHTML = html;
 }
 
-renderUsers();
+renderQuestions();
