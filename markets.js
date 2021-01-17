a6b474eb363981e8e131e28bfab72d8cce815702
Nitinram Velraj <nitinram1996@gmail.com>
2021-01-16 22:23:27 -0500

    markets now have css

--- markets.js
+++ markets.js
@@ -19,14 +19,33 @@ async function renderQuestions() {
     let questions = await getQuestions();
     let html = '';
     questions.forEach(question => {
-        // console.log(dateToDay(question.creation_time))
-        let htmlSegment = `<div class="well">
-        <h3 id="question">${question.question_text}</h3> 
-        <h6 id="explanation">${question.explanation_text}</h6>
-        <p id="resolution" class="glyphicon glyphicon-time">Question created on:${question.resolution_rule}</p>
-        <p id="time-created">Question created on: ${question.creation_time}</p>
-        <p id="deadline-bet">Deadline for placing bets: ${question.deadline_for_betting}</p>
-        <p id="deadline-resolve">Deadline for resolving question: ${question.deadline_for_resolving}</p>
+        // let htmlSegment = `<div class="blog-post">
+        // <h3 class="title" id="question">${question.question_text}</h3> 
+        // <h6 class="text" id="explanation">${question.explanation_text}</h6>
+        // <p class="user-info" id="resolution" class="glyphicon glyphicon-time">Resolution rule:${question.resolution_rule}</p>
+        // <p class="user-info" id="time-created">Question created on: ${question.creation_time}</p>
+        // <p class="user-info" id="deadline-bet">Deadline for placing bets: ${question.deadline_for_betting}</p>
+        // <p class="user-info" id="deadline-resolve">Deadline for resolving question: ${question.deadline_for_resolving}</p>
+        // </div>`;
+
+        let htmlSegment = `<div class="blog-post">
+        <h3 class="title" id="question">Who will win big boss season 4</h3> 
+
+        <h6 class="text" id="explanation">Ipsum faucibus vitae aliquet nec ullamcorper sit. 
+        Ac felis donec et odio pellentesque diam volutpat. Urna nunc id cursus metus aliquam. 
+        Amet dictum sit amet justo. Sem viverra aliquet eget sit amet tellus cras adipiscing. 
+        Urna nec tincidunt praesent semper. Integer enim neque volutpat ac. 
+        Aliquet enim tortor at auctor urna.</h6>
+
+        <p class="user-info" id="resolution" class="glyphicon glyphicon-time">Resolution rule:
+        Ipsum faucibus vitae aliquet nec ullamcorper sit. 
+        Ac felis donec et odio pellentesque diam volutpat. 
+        Urna nunc id cursus metus aliquam. 
+        Amet dictum sit amet justo.</p>
+
+        <p class="user-info" id="time-created">Question created on: ${question.creation_time}</p>
+        <p class="user-info" id="deadline-bet">Deadline for placing bets: ${question.deadline_for_betting}</p>
+        <p class="user-info" id="deadline-resolve">Deadline for resolving question: ${question.deadline_for_resolving}</p>
         </div>`;
 
         html += htmlSegment;
