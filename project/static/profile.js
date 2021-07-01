b49d16639b97b50f213d3518131938923303f176
Aditya-9712 <ak770@snu.edu.in>
2021-07-01 06:49:33 -0700

    profile.js

---
+++ project/static/profile.js
@@ -0,0 +1,73 @@
+// let loading = document.getElementById('loading')
+
+async function getQuestions() {
+    let url = 'https://5fff1998a4a0dd001701b79f.mockapi.io/questions';
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    };
+};
+
+async function dateToDay(date) {
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let dateSlice = new Date(date.slice(0,10));  
+    let day = days[dateSlice.getDay()];
+    return day
+};
+
+// window.addEventListener('scroll',()=>{
+//     const { scrollTop, scrollHeight, clientHeight} = document.documentElement;
+
+//     if (clientHeight+scrollTop >= scrollHeight-5){
+//         showLoading();
+//     }
+// });
+
+// function showLoading() {
+//     loading.classList.add('show');
+// }
+
+async function renderQuestions() {
+    let questions = await getQuestions();
+    let html = '';
+    questions.forEach(question => {
+        // let htmlSegment = `<div class="blog-post">
+        // <h3 class="title" id="question">${question.question_text}</h3> 
+        // <h6 class="text" id="explanation">${question.explanation_text}</h6>
+        // <p class="user-info" id="resolution" class="glyphicon glyphicon-time">Resolution rule:${question.resolution_rule}</p>
+        // <p class="user-info" id="time-created">Question created on: ${question.creation_time}</p>
+        // <p class="user-info" id="deadline-bet">Deadline for placing bets: ${question.deadline_for_betting}</p>
+        // <p class="user-info" id="deadline-resolve">Deadline for resolving question: ${question.deadline_for_resolving}</p>
+        // </div>`;
+
+        let htmlSegment = `<div class="blog-post">
+        <h3 class="title" id="question">Who will win big boss season 4</h3> 
+
+        <h6 class="text" id="explanation">Ipsum faucibus vitae aliquet nec ullamcorper sit. 
+        Ac felis donec et odio pellentesque diam volutpat. Urna nunc id cursus metus aliquam. 
+        Amet dictum sit amet justo. Sem viverra aliquet eget sit amet tellus cras adipiscing. 
+        Urna nec tincidunt praesent semper. Integer enim neque volutpat ac. 
+        Aliquet enim tortor at auctor urna.</h6>
+
+        <p class="user-info" id="resolution" class="glyphicon glyphicon-time">Resolution rule:
+        Ipsum faucibus vitae aliquet nec ullamcorper sit. 
+        Ac felis donec et odio pellentesque diam volutpat. 
+        Urna nunc id cursus metus aliquam. 
+        Amet dictum sit amet justo.</p>
+
+        <p class="user-info" id="time-created">Question created on: ${question.creation_time.slice(0,10)} at 4 pm IST</p>
+
+        <p class="user-info" id="deadline-bet">Deadline for placing bets: ${question.deadline_for_betting.slice(0,10)} at 4 pm IST</p>
+        <p class="user-info" id="deadline-resolve">Deadline for resolving question: ${question.deadline_for_resolving.slice(0,10)} at 4 pm IST</p>
+        </div>`;
+
+        html += htmlSegment;
+    });
+
+    let container = document.getElementById('marketList');
+    container.innerHTML = html;
+}
+
+renderQuestions();
